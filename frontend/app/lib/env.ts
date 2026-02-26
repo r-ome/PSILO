6a956aa1cbba3b748fe9af9d658aab651743b0bb
Jerome Agapay <jerome.arceo.agapay@gmail.com>
2026-02-26 16:54:09 +0800

    feat: removed client issuer on cognito, all api calls will be handled by the backend

--- frontend/app/lib/env.ts
+++ frontend/app/lib/env.ts
@@ -5,7 +5,6 @@ const envSchema = z.object({
   NEXT_PUBLIC_COGNITO_USER_POOL_ID: z.string().min(1),
   NEXT_PUBLIC_COGNITO_APP_CLIENT_ID: z.string().min(1),
   COGNITO_CLIENT_SECRET: z.string().min(1),
-  COGNITO_ISSUER: z.string().min(1),
   AWS_REGION: z.string().min(1),
 });
 
